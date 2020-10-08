7ea1480756d93ce4bf203107e86b1f192f3934a0
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-10-08 04:12:33 +0100

    fitching data from api users

--- src/index.js
+++ src/index.js
@@ -1,44 +1,13 @@
-import React, {Component} from "react"
-import ReactDOm from "react-dom"
+import React from "react"
+import ReactDOM from "react-dom"
+import  App from './App';
 
-let skiDay ={
-  total:20,
-  powder:10,
-  country: 98,
-  goal:3
-}
 
-class SkiDayCounter extends React.Component{
-  
-  render(){
-    const {total, powder, country, goal}= this.props
-    return(
-      <section>
-        <div>
-          <p> Total {total} </p>
-        </div>
-        <div>
-          <p>Powder; {powder}</p>
-        </div>
-        <div>
-          <p> Country : {country}</p>
-        </div>
-        <div>
-          <p>Goal : {goal}</p>
-        </div>
-      </section>
-    )
-  }
-}
 
-ReactDOm.render(
-  <>
-  <SkiDayCounter
-    total={skiDay.total}
-    powder={skiDay.powder}
-    country={skiDay.country}
-    goal={skiDay.goal}
-  />
-  </>,
+
+
+
+ReactDOM.render(
+ <App/>,
   document.getElementById('root')
 )
